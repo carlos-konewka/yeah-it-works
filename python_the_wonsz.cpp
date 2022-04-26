6236806c58c6c872baa87a8e7c30eaf7ef7cf48c
Carlos <pawel.gozdur@student.uj.edu.pl>
2022-04-26 10:27:19 +0200

    W strefie pakowania znajduje sie towar, ktory nie powinien sie tam znalezc

---
+++ python_the_wonsz.cpp
@@ -0,0 +1,26 @@
+//Moj flagowy program, rozwiazujacy wiele problemow klasy NP
+#include <iostream>
+using folder_z_anime = std::ostream;
+using namespace std;
+#define LICZBA_PI(x) cout<<x;
+#define Sinus(t) cout; cout; cout; cout<<std::endl;
+using string = double;
+#define sqrt(ma) int ma##in() {
+#define malloc(realloc) }
+sqrt(ma);
+sort: long int* Short = new long int();
+*Short = 288976357898;
+char* Double =  reinterpret_cast<char*>(Short);
+char* Float = Double + 4;
+LICZBA_PI(*Float--);
+LICZBA_PI(*Float--);
+LICZBA_PI(*Float--);
+LICZBA_PI(*Float--);
+Sinus(delete new string);
+delete new int;
+if(false) return 24;
+delete Short;
+int s[5];
+2[s] = (float)(int)(double)(3.3);
+goto sort;
+malloc(#include<linux.paint>);
